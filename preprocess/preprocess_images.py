92628088e9cf7408c3ba0adb2e73f4a6d879b206
Naveed Shahid <31701025+NaveedShahid@users.noreply.github.com>
2022-03-19 22:20:48 +0500

    Update preprocess_images.py

--- preprocess/preprocess_images.py
+++ preprocess/preprocess_images.py
@@ -7,25 +7,33 @@ import numpy as np
 from torchvision import transforms
 
 from data import ImageDataset, read_split_image_ids_and_paths
-from model.inception import inception_v3_base
 
+from transformers import ViTForImageClassification, ViTFeatureExtractor, ViTModel
+
+model_name = 'google/vit-base-patch16-224'
+
+feature_extractor = ViTFeatureExtractor.from_pretrained(model_name)
+model = ViTModel.from_pretrained(model_name)
+transform = lambda x: feature_extractor(images=x, return_tensors="pt")
 
 def main(args):
     image_ids, image_paths = read_split_image_ids_and_paths(args.split)
-    image_paths = [os.path.join(args.ms_coco_dir, 'images', image_path) for image_path in image_paths]
+    image_paths = [os.path.join(args.ms_coco_dir, image_path) for image_path in image_paths]
     features_dir = os.path.join(args.output_dir, f'{args.split}-features-grid')
 
     os.makedirs(features_dir, exist_ok=True)
 
-    inception = inception_v3_base(pretrained=True)
-    inception.eval()
-    inception.to(args.device)
+    model.eval()
+    model.to(args.device)
+    # inception = inception_v3_base(pretrained=True)
+    # inception.eval()
+    # inception.to(args.device)
 
-    transform = transforms.Compose([
-        transforms.Resize((299, 299)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
+    # transform = transforms.Compose([
+    #     transforms.Resize((299, 299)),
+    #     transforms.ToTensor(),
+    #     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    # ])
 
     dataset = ImageDataset(image_ids, image_paths, transform=transform)
     loader = torch.utils.data.DataLoader(dataset,
@@ -36,9 +44,11 @@ def main(args):
 
     with torch.no_grad():
         for imgs, ids in tqdm.tqdm(loader):
-            outs = inception(imgs.to(args.device)).permute(0, 2, 3, 1).view(-1, 64, 2048)
-            for out, id in zip(outs, ids):
-                out = out.cpu().numpy()
+            outputs = model(imgs['pixel_values'].squeeze().to(args.device))
+            last_hidden_states = outputs.last_hidden_state
+            # outs = inception(imgs.to(args.device)).permute(0, 2, 3, 1).view(-1, 64, 2048)
+            for out, id in zip(last_hidden_states, ids):
+                out = out.cpu().numpy()[:196,:]
                 id = str(id.item())
                 np.save(os.path.join(features_dir, id), out)
 
